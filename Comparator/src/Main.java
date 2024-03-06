748f11648a1ab99b9e3e766c26446ebe870274a2
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-06 12:17:20 -0300

    primeiro commit

---
+++ Comparator/src/Main.java
@@ -0,0 +1,13 @@
+import entities.Product;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product p1 = new Product("TV", 900.0);
+        Product p2 = new Product("PC", 1500.0);
+        Product p3 = new Product("Air Condicionado", 500.0);
+        Product p4 = new Product("Geladeira", 400.0);
+
+
+    }
+}
